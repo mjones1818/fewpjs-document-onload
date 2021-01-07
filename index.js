d0d144c314be03b95177cf2bec75a313bb0c650d
mjones1818 <michael.jones0532@gmail.com>
2021-01-07 08:55:32 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  textToModify = document.getElementById("text")
+  textToModify.innerHTML = "This is really cool!"
+});
+
